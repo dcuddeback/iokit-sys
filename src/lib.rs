75256f6b9230ac07336d148c52a0b4b427fe4a54
fort <fort@tmail.com>
2017-01-09 21:11:37 -0800

    Add hid manager definitions

--- src/lib.rs
+++ src/lib.rs
@@ -19,12 +19,14 @@ pub use keys::*;
 pub use serial::*;
 pub use types::*;
 pub use usb::*;
+pub use hid_manager::*;
 
 mod io_return;
 mod keys;
 mod serial;
 mod types;
 mod usb;
+mod hid_manager;
 
 // exports from <IOKit/IOKitLib.h>
 
