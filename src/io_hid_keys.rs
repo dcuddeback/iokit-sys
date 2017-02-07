86992ea5c123ebbda9e9ac62848de11f25b37ba1
fort <fort@tmail.com>
2017-02-07 01:03:54 -0800

    Change enum typedef

--- src/io_hid_keys.rs
+++ src/io_hid_keys.rs
@@ -1,14 +1,14 @@
 // exports from <IOKit/hid/IOHIDKeys.h>
 
-use libc::c_int;
+use types::IOOptionBits;
 
-pub type IOHIDReportType = c_int;
+pub type IOHIDReportType = IOOptionBits;
 pub const kIOHIDReportTypeInput: IOHIDReportType   = 0;
 pub const kIOHIDReportTypeOutput: IOHIDReportType  = 1;
 pub const kIOHIDReportTypeFeature: IOHIDReportType = 2;
 pub const kIOHIDReportTypeCount: IOHIDReportType   = 3;
 
-pub type IOHIDManagerOptions = c_int;
+pub type IOHIDManagerOptions = IOOptionBits;
 pub const kIOHIDManagerOptionNone: IOHIDManagerOptions                    = 0;
 pub const kIOHIDManagerOptionUsePersistentProperties: IOHIDManagerOptions = 1;
 pub const kIOHIDManagerOptionDoNotLoadProperties: IOHIDManagerOptions     = 2;
