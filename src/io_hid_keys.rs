ddbd6498c084d4bb9e11bcbb696109e679e7480c
David Cuddeback <david.cuddeback@gmail.com>
2017-03-19 08:44:16 -0700

    Fix enum values

--- src/io_hid_keys.rs
+++ src/io_hid_keys.rs
@@ -3,7 +3,7 @@
 use libc::c_int;
 
 pub type IOHIDReportType = c_int;
-pub const kIOHIDReportTypeInput: IOHIDReportType   = 0x0;
-pub const kIOHIDReportTypeOutput: IOHIDReportType  = 0x1;
-pub const kIOHIDReportTypeFeature: IOHIDReportType = 0x2;
-pub const kIOHIDReportTypeCount: IOHIDReportType   = 0x3;
+pub const kIOHIDReportTypeInput: IOHIDReportType   = 0;
+pub const kIOHIDReportTypeOutput: IOHIDReportType  = 1;
+pub const kIOHIDReportTypeFeature: IOHIDReportType = 2;
+pub const kIOHIDReportTypeCount: IOHIDReportType   = 3;
