6e721d0cfb074aa9db33c7787980c2739e38eb78
Phil Dennis-Jordan <phil@philjordan.eu>
2017-07-02 14:39:28 +0200

    Adds IONotificationPortGetRunLoopSource function & notification keys
    The notification keys are constants to be passed as the notificationType argument to IOServiceAddMatchingNotification().

--- src/keys.rs
+++ src/keys.rs
@@ -5,3 +5,18 @@ use libc::{c_char};
 pub fn kIOServiceClass() -> *const c_char {
     b"IOService\0".as_ptr() as *const c_char
 }
+pub fn kIOPublishNotification() -> *const c_char{
+	b"IOServicePublish\0".as_ptr() as *const c_char
+}
+pub fn kIOFirstPublishNotification() -> *const c_char{
+	b"IOServiceFirstPublish\0".as_ptr() as *const c_char
+}
+pub fn kIOMatchedNotification() -> *const c_char{
+	b"IOServiceMatched\0".as_ptr() as *const c_char
+}
+pub fn kIOFirstMatchNotification() -> *const c_char{
+	b"IOServiceFirstMatch\0".as_ptr() as *const c_char
+}
+pub fn kIOTerminatedNotification() -> *const c_char{
+	b"IOServiceTerminate\0".as_ptr() as *const c_char
+}
